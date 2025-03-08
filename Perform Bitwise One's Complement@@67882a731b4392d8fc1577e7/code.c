a97b28a4095054578cc1cbea42a5f67bdff07de0
Aditya Kumar maurya <aditya0248.becse24@chitkara.edu.in>
2025-03-08 10:45:28 +0530

    Solution to Perform Bitwise One's Complement

---
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -0,0 +1,11 @@
+// Your code here...
+#include <stdio.h>  
+
+int main() {  
+    int number, result;   
+    scanf("%d", &number);  
+    result = ~number;  
+    printf("%d\n",result);  
+    
+    return 0;  
+}  
